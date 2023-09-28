342d33ab5fcc239d176156ad10ff984873ef4f9f
MMMatth <mat.loup04@gmail.com>
2023-09-28 16:58:45 +0200

    ajout des poissons et supressions des poissons

--- glaces/Jeu.java
+++ glaces/Jeu.java
@@ -1,22 +1,31 @@
 package glaces;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jeu {
     private ArcticImage image;
     private Ocean ocean;
     private Pingouin pingouin;
+    private ArrayList<Poisson> poissons;
 
     public Jeu() {
         ocean = new Ocean();
-        pingouin = new Pingouin(ocean.getWidth() / 2, ocean.getHeight() / 2, 14); // on met le pingouin au milieu
+        pingouin = new Pingouin(30, 30, 14); // on met le pingouin au milieu
         image = new ArcticImage(ocean.getWidth(), ocean.getHeight()); // on cree l'image
-        image.setColors(creeCarte(ocean.getWidth(), ocean.getHeight(), ocean, pingouin)); // on met les couleurs
+        poissons = new ArrayList<Poisson>();
+
+        for (int i = 0; i < 20; i++) {
+            poissons.add(new Poisson(ocean.getWidth() - 1, ocean.getHeight() - 1, 5, 10));
+        }
+
+        image.setColors(creeCarte(ocean.getWidth(), ocean.getHeight(), ocean, pingouin, poissons)); // on met les couleurs
     }
 
     public void jouer() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            
             System.out.println("Deplacez le pingouin avec les touches zqsd (ou tapez une autre touche pour quitter) :");
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("z")) { // ingore case dans le cas de la maj
@@ -30,8 +39,12 @@ public class Jeu {
             } else {
                 break;
             }
-            
-            image.setColors(creeCarte(ocean.getWidth(), ocean.getHeight(), ocean, pingouin));
+            for (int i = 0; i < poissons.size(); i++) {
+                if (poissons.get(i).estMange(pingouin) && ) {
+                    poissons.remove(i);
+                }
+            }
+            image.setColors(creeCarte(ocean.getWidth(), ocean.getHeight(), ocean, pingouin, poissons));
         }
     }
     /**
@@ -47,35 +60,52 @@ public class Jeu {
         }
     }
 
-    private int[][] creeCarte(int mapWidth, int mapHeight, Ocean ocean, Pingouin pingouin) {
+    private int[][] creeCarte(int mapWidth, int mapHeight, Ocean ocean, Pingouin pingouin, ArrayList<Poisson> poissons) {
         int[][] carte = new int[mapWidth][mapHeight];
-        for (int i = 0; i < mapWidth; i++) {
-            for (int j = 0; j < mapHeight; j++) {
-                carte[i][j] = 0; // on met de l'eau de base
-                // on met de la glace si on est dans un iceberg
-                for (Iceberg2D iceberg : ocean.getIcebergs()) {
-                    if (iceberg.coinEnBasAGauche().getAbscisse() <= i
-                        && iceberg.coinEnBasAGauche().getOrdonnee() <= j
-                        && iceberg.coinEnHautADroite().getAbscisse() > i
-                        && iceberg.coinEnHautADroite().getOrdonnee() > j) 
-                    {
-                        carte[i][j] = 1;
-                    }
-                }
-                // on met le pingouin si on est dans le pingouin
-                if (pingouin.getX() <= i 
-                    && i < pingouin.getX() + pingouin.getTaille()
-                    && pingouin.getY() <= j 
-                    && j < pingouin.getY() + pingouin.getTaille()) {
-                    carte[i][j] = 3;
-                }
-
-
-            }
-        }
+        ajouterPoissons(carte, poissons);
+        ajouterIceberg(carte, ocean);
+        ajouterPingouin(carte, pingouin);
         return carte;
     }
 
+    private void ajouterIceberg(int[][] carte, Ocean ocean) {
+        for (Iceberg2D iceberg : ocean.getIcebergs()) {
+            int x1 = (int) iceberg.coinEnBasAGauche().getAbscisse();
+            int y1 = (int) iceberg.coinEnBasAGauche().getOrdonnee();
+            int x2 = (int) iceberg.coinEnHautADroite().getAbscisse();
+            int y2 = (int) iceberg.coinEnHautADroite().getOrdonnee();
+            for (int i = x1; i < x2; i++) {
+                for (int j = y1; j < y2; j++) {
+                    carte[i][j] = 1; // on met la couleur de l'iceberg
+                }
+            }
+        }
+    }
+
+    private void ajouterPingouin(int[][] carte, Pingouin pingouin) {
+        for (int i = pingouin.getX(); i < pingouin.getX() + pingouin.getTaille(); i++) {
+            for (int j = pingouin.getY(); j < pingouin.getY() + pingouin.getTaille(); j++) {
+                carte[i][j] = 3;
+            }
+        }
+    }
+
+    private void ajouterPoissons(int[][] carte, ArrayList<Poisson> poissons) {
+        for (Poisson poisson : poissons) {
+            if (poisson.getDirection() == 0) // horizontal
+                for (int i = poisson.getX(); i < poisson.getX() + poisson.getLargeur(); i++) {
+                    for (int j = poisson.getY(); j < poisson.getY() + poisson.getHauteur(); j++) {
+                        carte[i][j] = poisson.getCouleurs();
+                    }
+                }
+            else // vertical
+                for (int i = poisson.getX(); i < poisson.getX() + poisson.getHauteur(); i++) {
+                    for (int j = poisson.getY(); j < poisson.getY() + poisson.getLargeur(); j++) {
+                        carte[i][j] = poisson.getCouleurs();
+                    }
+                }
+        }
+    }
     public static void main(String[] args) {
         Jeu jeu = new Jeu();
         jeu.jouer();
--- glaces/Poisson.java
+++ glaces/Poisson.java
@@ -1,4 +1,9 @@
 package glaces;
+import geometrie.Point;
+
+import java.sql.Ref;
+// random
+import java.util.Random;
 
 public class Poisson {
     private int x;
@@ -6,26 +11,69 @@ public class Poisson {
     private int largeur;
     private int hauteur;
     private int direction; // 0 pour horizontal, 1 pour vertical
+    private int couleurs; // 4 ou 5
+    private int mapHeight;
+    private int mapWidth;
 
-    public Poisson(int x, int y, int largeur, int hauteur, int direction) {
-        this.x = x;
-        this.y = y;
+    public Poisson(int mapHeight, int mapWidth, int largeur, int hauteur) {
+        this.x = new Random().nextInt(mapWidth - 1);
+        this.y = new Random().nextInt(mapHeight- 1);
         this.largeur = largeur;
         this.hauteur = hauteur;
-        this.direction = direction;
+        this.mapHeight = mapHeight;
+        this.mapWidth = mapWidth;
+        if (this.x + largeur > mapWidth) this.x = mapWidth - largeur; // on verifie que le poisson ne sort pas de l'ocean
+        if (this.y + hauteur > mapHeight) this.y = mapHeight - hauteur;
+        
+        this.direction = new Random().nextInt(2);
+        this.couleurs = new Random().nextInt(2) + 4;
     }
 
-    public void deplacer(int dx, int dy, int largeurOcean, int hauteurOcean) {
-        if (direction == 0) {
-            x = (x + dx + largeurOcean) % largeurOcean;
+    public void refreshPosPoisson(int vitesse){
+        if (direction == 0) // si le poisson est vertical
+            {
+                if (y + (hauteur + vitesse)  >= mapHeight)
+                {
+                    y = 0;
+                }
+            }
+        else
+            {            
+                if (x + (hauteur + vitesse) >= mapWidth)
+                {
+                    x = 0;
+                }
+            }
+        
+    }
+
+    public void deplacer(int vitesse, int largeurOcean, int hauteurOcean) {
+        refreshPosPoisson(vitesse);
+
+        if (direction == 1) {
+            x = (x + vitesse) ;
         } else {
-            y = (y + dy + hauteurOcean) % hauteurOcean;
+            y = (y + vitesse) ;
         }
+        refreshPosPoisson(vitesse);
     }
 
     public boolean estMange(Pingouin pingouin) {
-        return (pingouin.getX() >= x && pingouin.getX() < x + largeur &&
-                pingouin.getY() >= y && pingouin.getY() < y + hauteur);
+        if (getDirection() == 1 )
+        {
+            if (pingouin.getX() < x + hauteur && pingouin.getX() + pingouin.getTaille() > x && pingouin.getY() < y + largeur && pingouin.getY() + pingouin.getTaille() > y) 
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (pingouin.getX() < x + largeur && pingouin.getX() + pingouin.getTaille() > x && pingouin.getY() < y + hauteur && pingouin.getY() + pingouin.getTaille() > y) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int getX() {
@@ -46,5 +94,9 @@ public class Poisson {
 
     public int getDirection() {
         return direction;
+    }
+
+    public int getCouleurs() {
+        return couleurs;
     }
 }
